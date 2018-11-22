235214eeeb4d139452f704291950a5f4aee8db25
LighthawkKnight <lighthawkknight@gmail.com>
2018-11-21 22:44:18 -0800

    Added schema, package.json

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
